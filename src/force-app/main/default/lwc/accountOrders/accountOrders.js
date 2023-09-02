c5356d34623437e14ae0743119b020010e76e880
Roman Slobodian <rslobodian@tripadvisor.com>
2023-09-02 17:49:52 +0300

    UC#1 Create LWC complete. Next one to be started later today with its own start commit.

--- src/force-app/main/default/lwc/accountOrders/accountOrders.js
+++ src/force-app/main/default/lwc/accountOrders/accountOrders.js
@@ -3,37 +3,54 @@ import getOrders from '@salesforce/apex/AccountOrdersController.getOrders';
 
 
 export default class AccountOrders extends LightningElement {
+    /**
+     * @description recordId: Id of the currently opened Account record.
+     */
     @api recordId;
 
-    orderInfos
-
+    /**
+     * @description orderInfos: Property used to store details about
+     * Orders to be shown.
+     */
     @wire(getOrders, { accountId: '$recordId' })
-    wiredOrders({ error, data }) {
-        console.log('error', error);
-        console.log('data', data);
-        console.log(JSON.stringify(data));
-        // if (data) {
-        //     this.orderInfos = data;
-        // }
-    };
+    orderInfos;
 
+    /**
+     * @description title: Getter used to reactively render component title.
+     */
     get title() {
         return `Orders (${this.orderInfos?.data?.length || '0'})`;
     }
 
+    /**
+     * @description showSpinner: Getter used to conditionally render component load spinner
+     * while Orders data is being loaded.
+     */
     get showSpinner() {
-        return this.orderInfos && !Object.hasOwn(this.orderInfos, 'data');
+        return Object.hasOwn(this.orderInfos || {}, 'data');
     }
 
+    /**
+     * @description showData: Getter used to conditionally render table with Order details
+     * (if there is any to display).
+     */
     get showData() {
         return 0 < this.orderInfos?.data?.length;
     }
 
+    /**
+     * @description showError: Getter used to conditionally render error message
+     * (if any returned).
+     */
     get showError() {
         return this.orderInfos?.error ? true : false;
     }
 
-    get showNoDataReturned() {
+    /**
+     * @description showNoRelatedOrdersMsg: Getter used to conditionally render notification
+     * message if there are no Orders related to given Account.
+     */
+    get showNoRelatedOrdersMsg() {
         return !this.showData && !this.showError && 0 >= this.orderInfos?.data?.length;
     }
 
