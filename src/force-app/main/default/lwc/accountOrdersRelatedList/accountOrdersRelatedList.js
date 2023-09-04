34f38d5fe54cbbc11f4d01bd26124f08fd6ee972
psavoss <psavo@softserveinc.com>
2023-09-04 14:33:45 +0300

    UC#1 Create LWC

---
+++ src/force-app/main/default/lwc/accountOrdersRelatedList/accountOrdersRelatedList.js
@@ -0,0 +1,31 @@
+import {api, LightningElement} from 'lwc';
+import getOrders from '@salesforce/apex/AccountOrdersController.getOrders'
+import {ShowToastEvent} from "lightning/platformShowToastEvent";
+
+export default class AccountOrdersRelatedList extends LightningElement {
+    @api recordId;
+
+    ordersTableData = [];
+
+    ordersTableColumnsDef = [{
+        type: 'text', fieldName: 'orderNumber', label: 'Number'
+    }, {
+        type: 'date', fieldName: 'startDate', label: 'Date'
+    }, {
+        type: 'text', fieldName: 'status', label: 'Status'
+    }, {
+        type: 'currency', fieldName: 'amount', label: 'Amount', typeAttributes: {currencyCode: 'USD'}
+    }];
+
+    connectedCallback() {
+        getOrders({accountId: this.recordId}).then(result => {
+            if (null !== result) {
+                this.ordersTableData = result;
+            }
+        }).catch(error => {
+            console.error('## error loading orders: ' + JSON.stringify(error));
+        })
+    }
+
+
+}
