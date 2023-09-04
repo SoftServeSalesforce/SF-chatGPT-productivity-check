be0526c34d9efce61012ba3ac7eeefa3b42a4e1a
Inna Kamenska <ikamen@softserveinc.com>
2023-09-04 10:32:58 +0300

    UC#1 Create LWC

--- src/force-app/main/default/lwc/accountOrdersRelatedList/accountOrdersRelatedList.js
+++ src/force-app/main/default/lwc/accountOrdersRelatedList/accountOrdersRelatedList.js
@@ -1,7 +1,46 @@
-import { LightningElement, api} from 'lwc';
+import { LightningElement, api, wire} from 'lwc';
+import getOrders from '@salesforce/apex/AccountOrdersController.getOrders';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+const columns = [
+    { label: 'Number', fieldName: 'orderLink', type: 'url' , typeAttributes: 
+    { label: { fieldName: "Number" }, tooltip:"Number", target: "_blank" }},
+    { label: 'Date', fieldName: 'Date', type: 'date' },
+    { label: 'Status', fieldName: 'Status', type: 'text' },
+    { label: 'Amount', fieldName: 'Amount', type: 'currency' }
+]
 
 export default class AccountOrdersRelatedList extends LightningElement {
 
     @api
     recordId;
+    result = []
+    columns = columns;
+   
+    @wire (getOrders, {accountId: '$recordId'})
+    orders({data, error}) {
+        if(data) {
+            this.result = data;
+            console.log(this.result);
+            let preparedOrders = [];
+            this.result.forEach(order => {
+                let preparedOrder = {};
+                preparedOrder.Id = order.id;
+                preparedOrder.orderLink = "/" + order.id;
+                preparedOrder.Number = order.orderNumber;
+                preparedOrder.Date = order.orderDate;               
+                preparedOrder.Status = order.status;
+                preparedOrder.Amount = order.amount;
+                preparedOrders.push(preparedOrder);
+        });
+        this.result = preparedOrders;
+        } else if (error) {
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: "Error!", 
+                    message: error.body.message, 
+                    variant: 'error'
+                })
+            );
+        }
+    }
 }
