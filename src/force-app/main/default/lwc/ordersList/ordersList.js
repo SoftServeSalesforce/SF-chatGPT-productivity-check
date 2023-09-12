0ef9d90188006355aff8266865acd82bd5a290ae
mpawlo <mpawlo@sofserveinc.com>
2023-09-12 16:26:33 +0200

    pause

--- src/force-app/main/default/lwc/ordersList/ordersList.js
+++ src/force-app/main/default/lwc/ordersList/ordersList.js
@@ -5,11 +5,22 @@ export default class OrdersList extends LightningElement {
     @api recordId;
 
     ordersData = [];
+
     ordersColumns = [
         { label: 'Number', fieldName: 'orderNumber', type: 'text' },
         { label: 'Date', fieldName: 'startDate', type: 'date' },
         { label: 'Status', fieldName: 'status', type: 'text' },
-        { label: 'Amount', fieldName: 'amount', type: 'currency', typeAttributes: {currencyCode: 'USD'} }
+        { label: 'Amount', fieldName: 'amount', type: 'currency', 
+            typeAttributes: {currencyCode: 'USD'} },
+        { type: 'button-icon', label: 'Invoice',
+            typeAttributes: {
+                iconName: 'utility:download',
+                title: 'Invoice',
+                variant: 'bare',
+                name: 'download_invoice',
+                disabled: {fieldName: 'hasInvoice'}
+            },},
+        { type: 'action', typeAttributes: { rowActions: this.getActions } }
     ];
 
     @wire(getOrders, {accountId: '$recordId'})
@@ -19,5 +30,26 @@ export default class OrdersList extends LightningElement {
         } else if (error) {
             console.error('error loading orders');
         }
+    }
+
+    getActions(row, doneCallback){
+        let actions = [];
+        if (row.status === 'Draft') {
+            actions.push({ label: 'Activate', name: 'activate'})
+        }
+        if (row.status === 'Activated') {
+            actions.push({ label: 'Mark as Shipped', name: 'markAsShipped'})
+        }
+        if (row.hasInvoice) {
+            actions.push({ label: 'Preview Invoice', name: 'previewInvoice'})
+        }
+        if (row.hasInvoice) {
+            actions.push({ label: 'Download Invoice', name: 'downloadInvoice'})
+        }
+        doneCallback(actions);
+    }
+
+    handleRowAction(){
+        
     }
 }
